fdd168eef1d050070986e937ce81a39ac97d9474
Alexandros Polymeros <alpolymer@gmail.com>
2025-05-25 17:35:01 +0300

    feat: Completed StringUtils methods

--- src/gr/aueb/cf/ch13/Main.java
+++ src/gr/aueb/cf/ch13/Main.java
@@ -1,7 +1,5 @@
 package gr.aueb.cf.ch13;
 
-import java.sql.Array;
-
 public class Main {
     public static void main(String[] args) {
         PointXYZ p1 = new PointXYZ(3,4,3);
@@ -12,8 +10,12 @@ public class Main {
         System.out.println("XYZ distance: " + p1.GetXYZDistance());
 
         int[] arr = {1,3,2,6,-2,3};
+        String s = "Anna";
         System.out.println(MathHelper.findMax(arr));//6
         System.out.println(MathHelper.findMin(arr));//-2
         System.out.println(MathHelper.getAverage(arr));
+        System.out.println(StringUtils.reverse(s));
+        System.out.println(StringUtils.isPalindrome(s));
+
     }
 }
--- src/gr/aueb/cf/ch13/StringUtils.java
+++ src/gr/aueb/cf/ch13/StringUtils.java
@@ -1,4 +1,21 @@
 package gr.aueb.cf.ch13;
 
 public class StringUtils {
+
+    private StringUtils(){
+
+    }
+
+    public static String reverse(String s){
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        return String.valueOf((sb.reverse()));
+    }
+
+    public static Boolean isPalindrome(String s){
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        sb.reverse();
+        return sb.toString().equalsIgnoreCase(s);
+    }
 }
