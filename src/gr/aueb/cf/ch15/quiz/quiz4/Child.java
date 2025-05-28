0ef6473cc2a2eed759dfc721a5a091449d3fde8d
Alexandros Polymeros <alpolymer@gmail.com>
2025-05-28 14:27:25 +0300

    feat: deleted quiz4

--- src/gr/aueb/cf/ch15/quiz/quiz4/Child.java
+++
@@ -1,5 +0,0 @@
-package gr.aueb.cf.ch15.quiz.quiz4;
-
-public class Child extends Parent{
-
-}
--- src/gr/aueb/cf/ch15/quiz/quiz4/Parent.java
+++
@@ -1,8 +0,0 @@
-package gr.aueb.cf.ch15.quiz.quiz4;
-
-public class Parent {
-
-    public void show(){
-        System.out.println("Parent");
-    }
-}
