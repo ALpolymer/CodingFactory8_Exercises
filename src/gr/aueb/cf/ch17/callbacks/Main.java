60e1661853c1eccf3c24dec45983ae01c9ceda4f
Alexandros Polymeros <alpolymer@gmail.com>
2025-06-08 12:25:05 +0300

    feat: add callbacks package

---
+++ src/gr/aueb/cf/ch17/callbacks/Main.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch17.callbacks;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void doPrint(Printable printable){
+        printable.print();
+    }
+}
