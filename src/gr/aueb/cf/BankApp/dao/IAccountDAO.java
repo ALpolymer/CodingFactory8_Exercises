cf9fbea6cf4150d370cd4273cb887bce2260da1d
Alexandros Polymeros <alpolymer@gmail.com>
2025-06-10 19:02:28 +0300

    feat: add IAccount interface]

--- src/gr/aueb/cf/BankApp/dao/IAccountDAO.java
+++ src/gr/aueb/cf/BankApp/dao/IAccountDAO.java
@@ -1,4 +1,14 @@
 package gr.aueb.cf.BankApp.dao;
 
+import gr.aueb.cf.BankApp.model.Account;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IAccountDAO {
+    void saveOrUpdate(Account account);
+    void remove(String iban);
+
+    Optional<Account> getByIban(String iban);
+    List<Account> getAccounts();
 }
