f4893acdd4b6d33e521d20a98551cdd881ca81db
Alexandros Polymeros <alpolymer@gmail.com>
2025-06-17 17:31:34 +0300

    feat: reluctant pattern

---
+++ src/gr/aueb/cf/ch19/regex/ReluctantPattern.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch19.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReluctantPattern {
+    public static void main(String[] args) {
+        String s = "login;alice;pass;12345;";
+        Pattern pattern = Pattern.compile(".*?gem;");
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
